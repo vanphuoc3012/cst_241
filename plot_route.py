93a87c9ffe7fe2f7c1f9e3626cb7f0c209d82702
phuocho <phuoc.ho@mservice.com.vn>
2024-11-10 12:52:29 +0700

    delete before rebase main

--- plot_route.py
+++ plot_route.py
@@ -5,4 +5,7 @@ import networkx as nx
 def plot_routes(graph, routes, **kwargs):
     if len(routes) == 1:
         return plot_graph_route(graph, routes[0], **kwargs)
+    
+    
+
     return plot_graph_routes(graph, routes, **kwargs)
